2edb0ac49f0e5b5c8365554a638740cbceec367d
Stefano Locati <stefano@obliquid.it>
2011-07-13 20:40:46 +0800

    Checkpointing the welcome servlet with Spring MVC, that now works

---
+++ src/main/java/com/apress/springrecipes/court/web/ReservationQueryController.java
@@ -0,0 +1,40 @@
+package com.apress.springrecipes.court.web;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.apress.springrecipes.court.service.ReservationService;
+import com.apress.springrecipes.court.domain.Reservation;
+
+@Controller
+@RequestMapping("/reservationQuery")
+public class ReservationQueryController {
+
+    private ReservationService reservationService;
+
+    @Autowired
+    public void ReservationQueryController(ReservationService reservationService) {
+        this.reservationService = reservationService;
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public void setupForm() {
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public String submitForm(@RequestParam("courtName") String courtName, Model model) {
+        List<Reservation> reservations = java.util.Collections.emptyList();
+        if (courtName != null) {
+            reservations = reservationService.query(courtName);
+        }
+        model.addAttribute("reservation", reservations);
+        return "reservationQuery";
+    }
+
+}
